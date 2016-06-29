8dfd457ce880c7f3da24242443e8045dd67d1268
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2016-06-29 20:16:17 +0100

    Imperfect circle class

--- js/index.js
+++ js/index.js
@@ -1,5 +1,77 @@
 // Start of Tic Tac Toe JS
-'use strict;'
+
+'use strict';
+/* Imperfect circle using 4 cubic Bezier curves
+Inspired by http://bl.ocks.org/patricksurry/11087975
+Constructor parameter:
+  x, y: Coordinates of the circle (default to 0)
+  r: radius of the circle (default to 1)
+*/
+class ImperfectCirclePath {
+  // TODO Animation
+  constructor(x = 0, y = 0, r = 1) {
+    this._x = x;
+    this._y = y;
+    this._r = r;
+    this._dR = 0.05;
+    this._thetaStart = 2 * Math.PI * Math.random();
+    this._dTheta = 0.2;
+    this._ratio = 0.9;
+    this._angle = Math.PI * Math.random();
+    this._path = undefined;
+  }
+  get x() {return this._x;}
+  get y() {return this._y;}
+  get r() {return this._y;}
+  get path() {return this._path;}
+
+  /* Appends the path to a d3 svg selection and returns the path */
+  addPathToSvg(svg) {
+    if (this._path !== undefined) {
+      throw("Error: Path already added to an svg");
+    }
+    this._path = svg.append('path')
+      .attr('d', this._imperfectCirclePath(this._dR, this._thetaStart, this._dTheta))
+      .attr('transform', this._scaleAtAngle(this._ratio, this._angle) +  `translate(${this._x}, ${this._y})`);
+  }
+
+  _imperfectCirclePath(dR, thetaStart, dTheta) {
+    const c = 0.551915024494,
+      beta = Math.atan(c),
+      d = Math.sqrt(c * c + 1 * 1);
+    let r = 1,
+      theta = thetaStart,
+      path = 'M',
+      randomFn = Math.random;
+
+    // Move to P0
+    path += [r * Math.sin(theta), r * Math.cos(theta)];
+    // Control point P1
+    path += ' C' + [d * r * Math.sin(theta + beta), d * r * Math.cos(theta + beta)];
+
+    // For each quarter turn
+    for (let i = 0; i < 4; i++) {
+      // Angle overshoot/undershoot
+      theta += Math.PI / 2 * (1 + randomFn() * dTheta);
+      // Radius overshoot/undershoot
+      r *= (1 + randomFn() * dR);
+      // Control point P2
+      path += ' ' + (i ? 'S' : '') + [d * r * Math.sin(theta - beta),
+        d * r * Math.cos(theta - beta)
+      ];
+      // Control point P3
+      path += ' ' + [r * Math.sin(theta), r * Math.cos(theta)];
+    }
+    return path;
+  }
+
+  _scaleAtAngle(ratio, angle) {
+    angle *= 180 / Math.PI;
+    return 'rotate(' + angle + ') ' +
+      'scale(1, ' + ratio + ') ' +
+      'rotate(' + (-angle) + ')';
+  }
+}
 
 /* Imperfect circle of base radius 1 using 4 cubic Bezier curves.
 From http://bl.ocks.org/patricksurry/11087975
@@ -10,14 +82,14 @@ dTheta: The maximum overshoot/undershoot angle per quarter turn (in radians)
 Returns:
 The d attribute of an SVG path
 */
-function imperfectCirclePath(dR, thetaStart, dTheta) {
-  let c = 0.551915024494,
-      beta = Math.atan(c),
-      d = Math.sqrt(c * c + 1 * 1),
-      r = 1,
-      theta = thetaStart,
-      path = 'M',
-      randomFn = Math.random;
+function imperfectCirclePathFn(dR, thetaStart, dTheta) {
+  const c = 0.551915024494,
+    beta = Math.atan(c),
+    d = Math.sqrt(c * c + 1 * 1);
+  let r = 1,
+    theta = thetaStart,
+    path = 'M',
+    randomFn = Math.random;
 
   // Move to P0
   path += [r * Math.sin(theta), r * Math.cos(theta)];
@@ -32,7 +104,8 @@ function imperfectCirclePath(dR, thetaStart, dTheta) {
     r *= (1 + randomFn() * dR);
     // Control point P2
     path += ' ' + (i ? 'S' : '') + [d * r * Math.sin(theta - beta),
-                                    d * r * Math.cos(theta - beta)];
+      d * r * Math.cos(theta - beta)
+    ];
     // Control point P3
     path += ' ' + [r * Math.sin(theta), r * Math.cos(theta)];
   }
@@ -43,7 +116,7 @@ function imperfectCirclePath(dR, thetaStart, dTheta) {
 Parameters: start and end angles in radians
 Return: d attribute of SVG path */
 function imperfectLine(startAngle, endAngle) {
-  let xMid = -0.2 + 0.4*Math.random(),
+  let xMid = -0.2 + 0.4 * Math.random(),
     path = 'M-1,0 C'; // The line starts at (0, 0)
 
   // Control point P1
@@ -60,9 +133,9 @@ function imperfectLine(startAngle, endAngle) {
 Returns: The transoform attribute for the SVG */
 function scaleAtAngle(ratio, angle) {
   angle *= 180 / Math.PI;
-  return 'rotate(' + angle + ') '
-          + 'scale(1, ' + ratio + ')'
-          + 'rotate(' + (-angle) + ')';
+  return 'rotate(' + angle + ') ' +
+    'scale(1, ' + ratio + ')' +
+    'rotate(' + (-angle) + ')';
 }
 
 function rndSign() {
@@ -70,47 +143,54 @@ function rndSign() {
 }
 
 
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(e) {
   let svg = d3.select("#circle0"),
-    angle1 = 30 + 30*Math.random();
-    angle2 = -30 - 30*Math.random();
+    angle1 = 30 + 30 * Math.random(),
+    angle2 = -30 - 30 * Math.random(),
+    c1 = new ImperfectCirclePath(),
+    c2 = new ImperfectCirclePath(3,3);
+
   // Circle
+  c1.addPathToSvg(svg);
+  c2.addPathToSvg(svg);
+
+  console.log(c1.path);
+
+  c1.path.attr('filter', 'url(#chalkTexture)')
+    .attr('fill', 'none')
+    .attr('stroke', 'white')
+    .attr('stroke-width', '0.1');
+  c2.path.attr('filter', 'url(#chalkTexture)')
+    .attr('fill', 'none')
+    .attr('stroke', 'white')
+    .attr('stroke-width', '0.1');
+  // Line
   svg.append('path')
     .attr('fill', 'none')
     .attr('stroke', 'white')
     .attr('stroke-width', '0.1')
     .attr('d', function() {
-      return imperfectCirclePath(0.05, 2*Math.PI*Math.random(), 0.2, 0, 0);})
-    .attr('transform', function() {
-      return scaleAtAngle(0.9, Math.PI*Math.random());})
-    .attr('filter', 'url(#chalkTexture)');
-  svg.append('path')
-    .attr('fill', 'none')
-    .attr('stroke', 'white')
-    .attr('stroke-width', '0.1')
-    .attr('d', function(){
-      return imperfectLine(rndSign() * (0.1 + 0.1 * Math.random()) ,
-                          rndSign() * (0.1 + 0.1 * Math.random()));})
+      return imperfectLine(rndSign() * (0.1 + 0.1 * Math.random()),
+        rndSign() * (0.1 + 0.1 * Math.random()));
+    })
     .attr('transform', 'rotate(' + angle1 + ') scale(1.414)')
     .attr('filter', 'url(#chalkTexture)');
   svg.append('path')
     .attr('fill', 'none')
     .attr('stroke', 'white')
     .attr('stroke-width', '0.1')
-    .attr('d', function(){
-      return imperfectLine(rndSign() * (0.1 + 0.1 * Math.random()) ,
-                          rndSign() * (0.1 + 0.1 * Math.random()));})
+    .attr('d', imperfectLine(rndSign() * (0.1 + 0.1 * Math.random()),
+      rndSign() * (0.1 + 0.1 * Math.random())))
     .attr('transform', 'rotate(' + angle2 + ') scale(1.414)')
     .attr('filter', 'url(#chalkTexture)');
   // Horizontal game lines
-  for (let i=0; i < 4; i++) {
+  for (let i = 0; i < 4; i++) {
     svg.append('path')
       .attr('fill', 'none')
       .attr('stroke', 'white')
       .attr('stroke-width', '0.2')
-      .attr('d', function(){
-        return imperfectLine(rndSign() * (0.1 + 0.1 * Math.random()) ,
-                            rndSign() * (0.1 + 0.1 * Math.random()));})
+      .attr('d', imperfectLine(rndSign() * (0.1 + 0.1 * Math.random()),
+        rndSign() * (0.1 + 0.1 * Math.random())))
       .attr('transform', 'rotate(' + (i < 2 ? 0 : 90) + ') translate(0,' + (i % 2 ? 1.5 : -1.5) + ') scale(4.5, 1)')
       .attr('filter', 'url(#chalkTexture)');
   }
